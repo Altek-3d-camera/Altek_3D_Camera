ef2ee4e7c978f88bd5bc1c9f0c674fe76dbe498e
owinchiang <owinchiang@altek.com.tw>
2022-03-22 15:27:03 +0800

    add first version for altek's organization group

---
+++ README.md
@@ -0,0 +1,99 @@
+# Altek_3D_Camera_SDK
+<p align="center"><img src="doc/img/altek_SUNNY_M1 .jpg" width="70%" /><br><br></p>
+
+-----------------
+[![GitHub CI](../../actions/workflows/buildsCI.yaml/badge.svg?branch=development)](../../actions/workflows/buildsCI.yaml)
+
+## Build Code Guide on Windows10.
+
+Prerequisite :
+1. Install Cmake 
+https://cmake.org/download/ 
+Windows x64 Installer: Installer tool has changed. Uninstall CMake 3.4 or lower first! 
+------------------------------------------------------------------------------------------------------
+2. Install git 
+https://git-scm.com/download/win 
+
+------------------------------------------------------------------------------------------------------
+3. Install Visual Studio 2019 community 
+https://visualstudio.microsoft.com/zh-hant/thank-you-downloading-visual-studio/?sku=Community&rel=16 
+
+Compile code process:
+Choose input source code/output path and click configure button from Cmake-gui . After configure done without errors , click Generate button to generate librealsense2.sln.
+
+Open Visual Studio 2019 to load project with librealsense2.sln ,compile ,and execute it .
+
+
+
+## Overview
+**Altek_3D_Camera SDK 1.0** is a cross-platform library for Altek® AQ360™ depth cameras.
+
+
+The SDK allows depth and color streaming, and provides intrinsic and extrinsic calibration information.
+Developer kits containing the necessary hardware to use this library are available for purchase at [store.altek.com](https://store.altek.com.tw/qualcomm/).
+Information about the Altek_3D_Camera technology at [https://www.altek.com.tw/](https://store.altek.com.tw/qualcomm/)
+
+> :open_file_folder: Don't have access to a Altek_3D_Camera? Check-out [sample data](./doc/sample-data.md)
+
+## Special notice from Altek™ regarding the recent press announcement
+
+Altek has decided to focus on AI Camera business and is announcing IoT-related product, and Tracking product lines this year. Altek will continue to provide Stereo products to its current distribution customers.  Altek will focus our new development on advancing innovative technologies that better support our core businesses.
+The products identified in this notification will be discontinued and unavailable for additional orders after Feb 28, 2022.
+
+The following Stereo Product Lines will continue to be supported: AL6100 depth cameras. 
+
+We will also continue the work to support and develop our Altek open source SDK.
+
+In the coming future Altek and AL6100 team will focus our new development on advancing innovative technologies that better support our core businesses and 
+
+
+## Download and Install
+* **Download** - The latest releases including Altek_3D_Camera SDK, Viewer and Depth Quality tools are available at: [**latest releases**]. Please check the [**release notes**] for the supported platforms, new features and capabilities, known issues, how to upgrade the Firmware and more.
+
+* **Install** - You can also install or build from source the SDK (on [Linux](./doc/distribution_linux.md) \ [Windows](./doc/distribution_windows.md) ), connect your AL6100 depth camera and you are ready to start writing your first application.
+
+> **Support & Issues**: If you need product support (e.g. ask a question about / are having problems with the device), please check the [FAQ & Troubleshooting](https://github.com/IntelRealSense/librealsense/wiki/Troubleshooting-Q%26A) section.
+> If not covered there, please search our [Closed GitHub Issues](https://github.com/IntelRealSense/librealsense/issues?utf8=%E2%9C%93&q=is%3Aclosed) page,  [Community](https://communities.intel.com/community/tech/realsense) and [Support](https://www.intel.com/content/www/us/en/support/emerging-technologies/intel-realsense-technology.html) sites.
+> If you still cannot find an answer to your question, please [open a new issue](https://github.com/IntelRealSense/librealsense/issues/new).
+
+
+
+## Ready to Hack!
+
+Our library offers a high level API for using Altek depth cameras (in addition to lower level ones).
+The following snippet shows how to start streaming frames and extracting the depth value of a pixel:
+
+```cpp
+// Create a Pipeline - this serves as a top-level API for streaming and processing frames
+rs2::pipeline p;
+
+// Configure and start the pipeline
+p.start();
+
+while (true)
+{
+    // Block program until frames arrive
+    rs2::frameset frames = p.wait_for_frames();
+
+    // Try to get a frame of a depth image
+    rs2::depth_frame depth = frames.get_depth_frame();
+
+    // Get the depth frame's dimensions
+    float width = depth.get_width();
+    float height = depth.get_height();
+
+    // Query the distance from the camera to the object in the center of the image
+    float dist_to_center = depth.get_distance(width / 2, height / 2);
+
+    // Print the distance
+    std::cout << "The camera is facing an object " << dist_to_center << " meters away \r";
+}
+```
+For more information on the library, please follow our [examples](./examples), and read the [documentation](./doc) to learn more.
+
+## Contributing
+In order to contribute to Altek SDK, please follow our [contribution guidelines](CONTRIBUTING.md).
+
+## License
+This project is licensed under the [Apache License, Version 2.0](LICENSE).
+Copyright 2018 Intel Corporation
---
+++ src/proc/spatial-filter.h
@@ -0,0 +1,324 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2017 Intel Corporation. All Rights Reserved
+//// In-place Domain Transform Edge-preserving filter based on
+// http://inf.ufrgs.br/~eslgastal/DomainTransform/Gastal_Oliveira_SIGGRAPH2011_Domain_Transform.pdf
+// The filter also allows to apply holes filling extention that due to implementation constrains can be applied horizontally only
+
+#pragma once
+
+#include <map>
+#include <vector>
+#include <cmath>
+
+#include "../include/librealsense2/hpp/rs_frame.hpp"
+#include "../include/librealsense2/hpp/rs_processing.hpp"
+
+#define _ALTEK_SF_ 1
+#define _ALTEK_SF_VERSION_ V1.0
+
+namespace librealsense
+{
+    class spatial_filter : public depth_processing_block
+    {
+    public:
+        spatial_filter();
+
+    protected:
+        void    update_configuration(const rs2::frame& f);
+
+        rs2::frame prepare_target_frame(const rs2::frame& f, const rs2::frame_source& source);
+        rs2::frame process_frame(const rs2::frame_source& source, const rs2::frame& f) override;
+
+        template <typename T>
+		void dxf_smooth(void *frame_data, float alpha, float delta, int iterations)
+		{
+#if _ALTEK_SF_
+			if (_extension_type == RS2_EXTENSION_DEPTH_FRAME)
+			{
+				altek_spatial_filter(frame_data, alpha, delta, iterations);
+			}
+#else
+			static_assert((std::is_arithmetic<T>::value), "Spatial filter assumes numeric types");
+			bool fp = (std::is_floating_point<T>::value);
+			for (int i = 0; i < iterations; i++)
+			{
+				if (fp)
+				{
+					recursive_filter_horizontal_fp(frame_data, alpha, delta);
+					recursive_filter_vertical_fp(frame_data, alpha, delta);
+				}
+				else
+				{
+					recursive_filter_horizontal<T>(frame_data, alpha, delta);
+					recursive_filter_vertical<T>(frame_data, alpha, delta);
+				}
+			}
+			// Disparity domain hole filling requires a second pass over the frame data
+			// For depth domain a more efficient in-place hole filling is performed
+			if (_holes_filling_mode && fp)
+				intertial_holes_fill<T>(static_cast<T*>(frame_data));
+		#endif
+		}
+		
+		void recursive_filter_horizontal_fp(void * image_data, float alpha, float deltaZ);
+		void recursive_filter_vertical_fp(void * image_data, float alpha, float deltaZ);
+
+#if _ALTEK_SF_
+//----------ALTEK Spatial Filter Function define----------------------- 
+		//define min kit distance estimation
+		#define _ALTEK_SF_MIN_DIST_ 100     
+		//define max Mean Difference Check Threshold (for disparity)
+		#define _ALTEK_SF_DELTA_MAX_ 10   
+		//spatial filter main fuction
+		void altek_spatial_filter(void * image_data, float alpha, float deltaZ, int iterations);                                                                                                                    
+		//sub function, initial Mean Difference Check Threshold LUT
+		void _altek_sf_mdc_th_init(uint16_t* spatial_delta_LUT);                                                                                                                                                                   
+		//sub function, do Mean Difference Check
+		void _altek_sf_mdc(uint16_t* image, uint16_t* spatial_delta_LUT, int* timage, int* cimage, int mask_w_h, int mask_h_h, int width2, int height2);    
+		//sub function, do Density Check 	
+		void _altek_sf_dc(uint16_t * image, int* cimage, int iterations, int mask_w_h, int mask_h_h, int width2, int height2);                                                          
+//---------------------------------
+#endif
+
+        template <typename T>
+        void  recursive_filter_horizontal(void * image_data, float alpha, float deltaZ)
+        {
+            size_t v{}, u{};
+
+            // Handle conversions for invalid input data
+            bool fp = (std::is_floating_point<T>::value);
+
+            // Filtering integer values requires round-up to the nearest discrete value
+            const float round = fp ? 0.f : 0.5f;
+            // define invalid inputs
+            const T valid_threshold = fp ? static_cast<T>(std::numeric_limits<T>::epsilon()) : static_cast<T>(1);
+            const T delta_z = static_cast<T>(deltaZ);
+
+            auto image = reinterpret_cast<T*>(image_data);
+            size_t cur_fill = 0;
+
+            for (v = 0; v < _height; v++)
+            {
+                // left to right
+                T *im = image + v * _width;
+                T val0 = im[0];
+                cur_fill = 0;
+
+                for (u = 1; u < _width - 1; u++)
+                {
+                    T val1 = im[1];
+
+                    if (fabs(val0) >= valid_threshold)
+                    {
+                        if (fabs(val1) >= valid_threshold)
+                        {
+                            cur_fill = 0;
+                            T diff = static_cast<T>(fabs(val1 - val0));
+
+                            if (diff >= valid_threshold && diff <= delta_z)
+                            {
+                                float filtered = val1 * alpha + val0 * (1.0f - alpha);
+                                val1 = static_cast<T>(filtered + round);
+                                im[1] = val1;
+                            }
+                        }
+                        else // Only the old value is valid - appy holes filling
+                        {
+                            if (_holes_filling_radius)
+                            {
+                                if (++cur_fill <_holes_filling_radius)
+                                    im[1] = val1 = val0;
+                            }
+                        }
+                    }
+
+                    val0 = val1;
+                    im += 1;
+                }
+
+                // right to left
+                im = image + (v + 1) * _width - 2;  // end of row - two pixels
+                T val1 = im[1];
+                cur_fill = 0;
+
+                for (u = _width - 1; u > 0; u--)
+                {
+                    T val0 = im[0];
+
+                    if (val1 >= valid_threshold)
+                    {
+                        if (val0 > valid_threshold)
+                        {
+                            cur_fill = 0;
+                            T diff = static_cast<T>(fabs(val1 - val0));
+
+                            if (diff <= delta_z)
+                            {
+                                float filtered = val0 * alpha + val1 * (1.0f - alpha);
+                                val0 = static_cast<T>(filtered + round);
+                                im[0] = val0;
+                            }
+                        }
+                        else // 'inertial' hole filling
+                        {
+                            if (_holes_filling_radius)
+                            {
+                                if (++cur_fill <_holes_filling_radius)
+                                    im[0] = val0 = val1;
+                            }
+                        }
+                    }
+
+                    val1 = val0;
+                    im -= 1;
+                }
+            }
+        }
+
+        template <typename T>
+        void recursive_filter_vertical(void * image_data, float alpha, float deltaZ)
+        {
+            size_t v{}, u{};
+
+            // Handle conversions for invalid input data
+            bool fp = (std::is_floating_point<T>::value);
+
+            // Filtering integer values requires round-up to the nearest discrete value
+            const float round = fp ? 0.f : 0.5f;
+            // define invalid range
+            const T valid_threshold = fp ? static_cast<T>(std::numeric_limits<T>::epsilon()) : static_cast<T>(1);
+            const T delta_z = static_cast<T>(deltaZ);
+
+            auto image = reinterpret_cast<T*>(image_data);
+
+            // we'll do one row at a time, top to bottom, then bottom to top
+
+            // top to bottom
+
+            T *im = image;
+            T im0{};
+            T imw{};
+            for (v = 1; v < _height; v++)
+            {
+                for (u = 0; u < _width; u++)
+                {
+                    im0 = im[0];
+                    imw = im[_width];
+
+                    //if ((fabs(im0) >= valid_threshold) && (fabs(imw) >= valid_threshold))
+                    {
+                        T diff = static_cast<T>(fabs(im0 - imw));
+                        if (diff < delta_z)
+                        {
+                            float filtered = imw * alpha + im0 * (1.f - alpha);
+                            im[_width] = static_cast<T>(filtered + round);
+                        }
+                    }
+                    im += 1;
+                }
+            }
+
+            // bottom to top
+            im = image + (_height - 2) * _width;
+            for (v = 1; v < _height; v++, im -= (_width * 2))
+            {
+                for (u = 0; u < _width; u++)
+                {
+                    im0 = im[0];
+                    imw = im[_width];
+
+                    if ((fabs(im0) >= valid_threshold) && (fabs(imw) >= valid_threshold))
+                    {
+                        T diff = static_cast<T>(fabs(im0 - imw));
+                        if (diff < delta_z)
+                        {
+                            float filtered = im0 * alpha + imw * (1.f - alpha);
+                            im[0] = static_cast<T>(filtered + round);
+                        }
+                    }
+                    im += 1;
+                }
+            }
+        }
+
+        template<typename T>
+        inline void intertial_holes_fill(T* image_data)
+        {
+            std::function<bool(T*)> fp_oper = [](T* ptr) { return !*((int *)ptr); };
+            std::function<bool(T*)> uint_oper = [](T* ptr) { return !(*ptr); };
+            auto empty = (std::is_floating_point<T>::value) ? fp_oper : uint_oper;
+
+            size_t cur_fill = 0;
+
+            T* p = image_data;
+            for (int j = 0; j < _height; ++j)
+            {
+                ++p;
+                cur_fill = 0;
+
+                //Left to Right
+                for (size_t i = 1; i < _width; ++i)
+                {
+                    if (empty(p))
+                    {
+                        if (++cur_fill < _holes_filling_radius)
+                            *p = *(p - 1);
+                    }
+                    else
+                        cur_fill = 0;
+
+                    ++p;
+                }
+
+                --p;
+                cur_fill = 0;
+                //Right to left
+                for (size_t i = 1; i < _width; ++i)
+                {
+                    if (empty(p))
+                    {
+                        if (++cur_fill < _holes_filling_radius)
+                            *p = *(p + 1);
+                    }
+                    else
+                        cur_fill = 0;
+                    --p;
+                }
+                p += _width;
+            }
+        }
+
+    private:
+
+        float                   _spatial_alpha_param;
+#if _ALTEK_SF_
+		float                 _spatial_delta_param;
+#else
+		uint8_t                 _spatial_delta_param;
+#endif
+        uint8_t                 _spatial_iterations;
+        float                   _spatial_edge_threshold;
+        size_t                  _width, _height, _stride;
+        size_t                  _bpp;
+        rs2_extension           _extension_type;            // Strictly Depth/Disparity
+        size_t                  _current_frm_size_pixels;
+        rs2::stream_profile     _source_stream_profile;
+        rs2::stream_profile     _target_stream_profile;
+        bool                    _stereoscopic_depth;
+        float                   _focal_lenght_mm;
+        float                   _stereo_baseline_mm;
+        uint8_t                 _holes_filling_mode;
+        uint8_t                 _holes_filling_radius;
+#if _ALTEK_SF_
+		int32_t      _spatial_delta_LUT_buffer_init_flag;        //if value=0, then malloc for Mean Difference Check Threshold LUT buffer.  if value=1, buffer ready.
+		int32_t      _spatial_delta_LUT_value_init_flag;         //if value=0, then set new value to Mean Difference Check Threshold LUT.  if value=1, Mean Difference Check Threshold LUT is ok.
+		uint16_t* _spatial_delta_LUT;                                        //Mean Difference Check Threshold LUT, need malloc memory (buffer size is 65536*sizeof(uint16)).
+		int32_t      _spatial_integralimage_buffer_init_flag; //if value=1, need free buffer then malloc new buffer for new integral image.
+		int32_t      _spatial_integralimage_size_pixels;          //log depth frame size, (integral image size based on frame size).
+		int32_t      _spatial_integralimage_mask_mode;       //log  spatial filter mask size, (integral image size based on mask size).
+		int32_t*    _spatial_count_integralimage;                   //integral image for valid count depth pixel, need malloc memory (buffer size is (depth width + mask width)* (depth height + mask height)*sizeof(int32).
+		int32_t*   _spatial_value_integralimage;                    // integral image for sum of valid depth value, need malloc memory (buffer size is (depth width + mask width)* (depth height + mask height)*sizeof(int32).
+#endif
+    };
+    MAP_EXTENSION(RS2_EXTENSION_SPATIAL_FILTER, librealsense::spatial_filter);
+}
